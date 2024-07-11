78a73c1b38b49d3b406a2c5b9ff3531f46151707
johnson lu <johnsonlu@outlook.com>
2024-07-11 16:23:32 +0800

    read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -25,7 +27,7 @@ public class Main {
 
             var httpRequest = parseHttpRequest(clientSocket);
 
-            var response = getString(httpRequest);
+            var response = getHttpResponse(httpRequest);
 
             clientSocket.getOutputStream().write(response);
         } catch (IOException e) {
@@ -33,11 +35,18 @@ public class Main {
         }
     }
 
-    private static byte[] getString(HttpRequest httpRequest) {
+    private static byte[] getHttpResponse(HttpRequest httpRequest) {
         String response;
 
         if (httpRequest.getTarget().equals("/")) {
             response = HTTP_200_EMPTY_RESPONSE;
+        } else if (httpRequest.getTarget().equalsIgnoreCase("/user-agent")) {
+            var content = httpRequest.getHeaders().getOrDefault("User-Agent", "");
+            response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/plain\r\n" +
+                    String.format("Content-Length: %d\r\n", content.length()) +
+                    "\r\n" +
+                    content;
         } else if (httpRequest.getTarget().startsWith("/echo/")) {
             var content = httpRequest.getTarget().substring(6);
             response = "HTTP/1.1 200 OK\r\n" +
@@ -62,6 +71,15 @@ public class Main {
         httpRequest.setMethod(requestLine[0]);
         httpRequest.setTarget(requestLine[1]);
         httpRequest.setVersion(requestLine[2]);
+
+        Map<String, String> headers = new HashMap<>();
+        for (int i = 1; i < lines.length; i++) {
+            var tokens = lines[i].split(":");
+            if (tokens.length == 2) {
+                headers.put(tokens[0].trim(), tokens[1].trim());
+            }
+        }
+        httpRequest.setHeaders(headers);
 
         return httpRequest;
     }
@@ -91,6 +109,7 @@ public class Main {
         private String method;
         private String target;
         private String version;
+        private Map<String, String> headers;
 
         public String getVersion() {
             return version;
@@ -114,6 +133,14 @@ public class Main {
 
         public void setMethod(String method) {
             this.method = method;
+        }
+
+        public Map<String, String> getHeaders() {
+            return headers;
+        }
+
+        public void setHeaders(Map<String, String> headers) {
+            this.headers = headers;
         }
     }
 }
