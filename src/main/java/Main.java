fd26d9981c3ee06aebf76ec8150010dd8c8f6fe0
johnson lu <johnsonlu@outlook.com>
2024-07-11 14:42:24 +0800

    respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+    private static final String HTTP_200_EMPTY_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -17,7 +20,9 @@ public class Main {
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
+
+            var outputStream = clientSocket.getOutputStream();
+            outputStream.write(HTTP_200_EMPTY_RESPONSE.getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
