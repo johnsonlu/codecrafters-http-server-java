edb5d2f1a31123bb42398b749f0bf8c086418bd9
johnson lu <johnsonlu@outlook.com>
2024-07-11 15:07:55 +0800

    extract url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     private static final String HTTP_200_EMPTY_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
+    private static final String HTTP_404_EMPTY_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n";
 
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,10 +23,78 @@ public class Main {
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
 
-            var outputStream = clientSocket.getOutputStream();
-            outputStream.write(HTTP_200_EMPTY_RESPONSE.getBytes());
+            var httpRequest = parseHttpRequest(clientSocket);
+
+            var response = httpRequest.getTarget().equals("/") ? HTTP_200_EMPTY_RESPONSE : HTTP_404_EMPTY_RESPONSE;
+
+            clientSocket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static HttpRequest parseHttpRequest(Socket socket) throws IOException {
+        var requestContent = getRequestContent(socket);
+
+        var lines = requestContent.split("\r\n");
+        var requestLine = parseRequestLine(lines[0]);
+
+        var httpRequest = new HttpRequest();
+        httpRequest.setMethod(requestLine[0]);
+        httpRequest.setTarget(requestLine[1]);
+        httpRequest.setVersion(requestLine[2]);
+
+        return httpRequest;
+    }
+
+    private static String[] parseRequestLine(String requestLine) {
+        return requestLine.split(" ");
+    }
+
+    private static String getRequestContent(Socket socket) throws IOException {
+        byte[] buffer = new byte[1024];
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int bytesRead = socket.getInputStream().read(buffer);
+            if (bytesRead > 0) {
+                sb.append(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8));
+            }
+            if (socket.getInputStream().available() == 0) {
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    static class HttpRequest {
+        private String method;
+        private String target;
+        private String version;
+
+        public String getVersion() {
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
+        public String getTarget() {
+            return target;
+        }
+
+        public void setTarget(String target) {
+            this.target = target;
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public void setMethod(String method) {
+            this.method = method;
         }
     }
 }
